ebfdba30f4e0d26fc481fe54715692c50f125574
RafM <rkjmosiolek@gmail.com>
2018-11-18 20:23:37 +0000

    prototype ready; fix gulpfile; fix package.json cause i am a moron; update gitignore; add tooltips; add multichoice buttons; reveal sections based on user actions; make it somewhat responsive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,41 @@
-const multichoiceBtns = document.querySelectorAll('.btn-multichoice');
+var multichoiceBtns = document.querySelectorAll('.btn-multichoice');
+var activeItems = [];
 
-alert('hello');
+for (var i = 0; i < multichoiceBtns.length; i++) {
+    multichoiceBtns[i].addEventListener("click", function(ev) {
+        for (var i = 0; i < multichoiceBtns.length; i++) {
+            checkIfAnalysisShouldBeRevealed();
+            if (this != multichoiceBtns[i]) {
+            } else if (this.classList.contains('active')) {
+                this.classList.toggle('active');
+            } else {
+                this.classList.toggle('active')
+                activeItems.push(this);
+            }
+        }
+        ev.preventDefault();
+    }, false);
+};
+
+
+var startBtn = document.getElementById('start-btn');
+var userInteraction = document.getElementById('user-interaction');
+var analysis = document.getElementById('analysis');
+
+startBtn.addEventListener('click', () => {
+    userInteraction.style.display = 'block';
+    smoothScrollToTheBottomOfThePageWithjQuery();
+})
+
+function checkIfAnalysisShouldBeRevealed() {
+    console.log('checkIfAnalysisShouldBeRevealed called');
+    if (activeItems.length > 2) {
+        analysis.style.display = 'block';
+        smoothScrollToTheBottomOfThePageWithjQuery();
+        
+    }
+}
+
+function smoothScrollToTheBottomOfThePageWithjQuery() {
+    $('html,body').animate({scrollTop: document.body.scrollHeight}, "slow");
+}
